9100b8e04f71b0f5fbf4ce11015bffc6af42c0f4
anesboz <anesboz3@gmail.com>
2022-09-07 16:34:16 +0200

    Wed Sep  7 16:34:16 CEST 2022

--- src/components/GMap/GMap.js
+++ src/components/GMap/GMap.js
@@ -1,7 +1,7 @@
-import { Button, CircularProgress } from '@mui/material'
+import { Button, CircularProgress, Grid } from '@mui/material'
 import { GContext } from 'App'
 import axios from 'axios'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import {
   MapContainer,
   TileLayer,
@@ -9,95 +9,128 @@ import {
   Polygon,
   Tooltip,
   Popup,
+  Rectangle,
+  useMap,
+  useMapEvent,
 } from 'react-leaflet'
 import { GET_ARRONDISSEMENT_URL } from 'variables/constants'
+import { getTemparature, getTrees, zoneToSquare } from './utils/utils'
+import Zone from './Zone'
+
+export const RAYON = 10 // km
+export const delta_lat = (r) => r / 110.574
+export const delta_long = (r, lat) => r / 111.32
+
+// export const delta_lat = (r) => r / 110.574
+// export const delta_long = (r, lat) => r / 111.32
 
 export default function GMap(props) {
-  // const [zoneDisplay, setZoneDisplay] = props.displayZonesHook
-  const center = [48.852174427333274, 2.299322310264648] // default par Paris
-  const [loading, setloading] = useState(true)
-  const [zones, setZones] = useState([])
-  const [selected, setSelected] = useState(0)
+  const defaultCenter = [48.852174427333274, 2.299322310264648] // default par Paris
+  function getMatrice(centerArr) {
+    const [lat, lon] = centerArr
+    const delta = RAYON
 
-  const { zonesDisplayHook } = useContext(GContext)
-  const [zonesDisplay, setZonesDisplay] = zonesDisplayHook
+    return [
+      [lat + delta_lat(delta), lon - delta_long(delta, lat)],
+      [lat + delta_lat(delta), lon],
+      [lat + delta_lat(delta), lon + delta_long(delta, lat)],
 
-  useEffect(() => {
-    axios
-      .get(GET_ARRONDISSEMENT_URL)
-      .then((res) => {
-        const data = res.data.records?.map((elt) =>
-          elt.fields.geom.coordinates[0].map((arr) => arr.reverse())
-        )
-        setZones(data)
-      })
-      .catch((err) => console.log(err))
-      .finally(() => setloading(false))
-  }, [])
-  useEffect(() => {
-    if (!zonesDisplay) setSelected(null)
-  }, [zonesDisplay])
+      [lat, lon - delta_long(delta, lat)],
+      [lat, lon],
+      [lat, lon + delta_long(delta, lat)],
 
-  if (loading) {
-    return <CircularProgress color="primary" />
+      [lat - delta_lat(delta), lon - delta_long(delta, lat)],
+      [lat - delta_lat(delta), lon],
+      [lat - delta_lat(delta), lon + delta_long(delta, lat)],
+    ]
   }
 
+  const [center, setCenter] = useState(defaultCenter)
+  const [dLat, setDLat] = useState(0)
+  const [dLng, setDLng] = useState(0)
+
+  const [points, setPoints] = useState([])
+
+  const matrice = getMatrice(center)
+
+  function SetViewOnClick({ animateRef }) {
+    const mMap = useMap()
+
+    const map = useMapEvent('zoomend drag', (e) => {
+      let newBounds = mMap.getBounds()
+      const { _northEast, _southWest } = newBounds
+
+      const l = _northEast.lat - _southWest.lat
+      const L = _northEast.lng - _southWest.lng
+
+      setPoints([
+        [_northEast.lat, _southWest.lng],
+        [_northEast.lat, _southWest.lng + L / 3],
+        [_northEast.lat, _southWest.lng + (2 * L) / 3],
+        [_northEast.lat, _northEast.lng],
+
+        [_southWest.lat + (2 * l) / 3, _southWest.lng],
+        [_southWest.lat + (2 * l) / 3, _southWest.lng + L / 3],
+        [_southWest.lat + (2 * l) / 3, _southWest.lng + (2 * L) / 3],
+        [_southWest.lat + (2 * l) / 3, _northEast.lng],
+
+        [_southWest.lat + l / 3, _southWest.lng],
+        [_southWest.lat + l / 3, _southWest.lng + L / 3],
+        [_southWest.lat + l / 3, _southWest.lng + (2 * L) / 3],
+        [_southWest.lat + l / 3, _northEast.lng],
+
+        [_southWest.lat, _southWest.lng],
+        [_southWest.lat, _southWest.lng + L / 3],
+        [_southWest.lat, _southWest.lng + (2 * L) / 3],
+        [_southWest.lat, _northEast.lng],
+      ])
+
+      const center_ = [
+        (_northEast.lat + _southWest.lat) / 2,
+        (_northEast.lng + _southWest.lng) / 2,
+      ]
+
+      setCenter(center_)
+      // map.setView(e.latlng, map.getZoom(), {
+      //   animate: animateRef.current || false,
+      // })
+    })
+
+    return null
+  }
+  const animateRef = useRef(false)
+
   return (
-    <MapContainer
-      center={center}
-      zoom={12}
-      style={{
-        minHeight: '90vh',
-        width: `100%`,
-        marginBottom: 0,
-      }}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {!zonesDisplay
-        ? null
-        : zones.map((e, i) => (
-            <Polygon
-              key={i}
-              index={i}
-              pathOptions={{
-                color: i === selected ? '#0000ff8f' : '#00800078',
-                fillColor: i === selected ? '#0000ff8f' : '#008000ab',
-                // className:
-                //   selected == null ? '' : (i === selected ? '' : 'no-display'),
-              }}
-              positions={e}
-              selectedHook={[selected, setSelected]}
-            >
-              <Popup>
-                <Button
-                  size="small"
-                  variant="contained"
-                  onClick={() => setSelected(i === selected ? null : i)}
-                >
-                  {i === selected ? `Unselect` : `Select`}
-                </Button>
-              </Popup>
-            </Polygon>
-          ))}
-
-      {/* <Zone
-        pathOptions={{ color: 'green' }}
-        positions={zones}
-        selectedHook={[selected, setSelected]}
-      >
-      </Zone> */}
-
-      <Circle
+    <Grid container>
+      <MapContainer
         center={center}
-        pathOptions={{ color: 'red', fillColor: 'green' }}
-        radius={200}
+        zoom={12}
+        style={{
+          minHeight: '90vh',
+          width: `100%`,
+          marginBottom: 0,
+        }}
+        scrollWheelZoom={true}
       >
-        <Tooltip>Tree</Tooltip>
-      </Circle>
-    </MapContainer>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <SetViewOnClick animateRef={animateRef} />
+        {/* {matrice.map((e, i) => {
+          return <Zone key={i} index={i} center={e} dLat={dLat} dLng={dLng} />
+        })} */}
+        {/* {points.map((e, i) => {
+          return <Circle center={e} radius={1000}></Circle>
+        })} */}
+        {[...Array(11).keys()].map((i) => {
+          if ([3, 7, 11].includes(i)) return null
+          const a = points[i]
+          const b = points[i + 5]
+          if (a == null) return null
+          return <Zone key={i} bounds={[a, b]} />
+        })}
+      </MapContainer>
+    </Grid>
   )
 }
